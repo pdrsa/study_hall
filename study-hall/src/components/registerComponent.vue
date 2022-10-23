5b998a9e421c7e2325e1ff0acdf4b06077b74c82
Heitor <heitor.santos@quasar.srv.br>
2022-10-23 16:24:35 -0300

    Some progress in register screen.

--- study-hall/src/components/registerComponent.vue
+++ study-hall/src/components/registerComponent.vue
@@ -3,33 +3,72 @@
     <v-card
       color="#FFFFFF"
       elevation=2
-      height="1100"
-      width="1188"
+      height="1100px"
+      width="1188px"
       rounded="xl"
     >
       <div>
         <v-row>
           <v-col
           cols="12"
-          lg="8">
+          lg="8"
+          class="pa-8" >
             <h1>Register</h1>
           </v-col>
         </v-row>
       </div>
-      <v-text-field filled label="Username"></v-text-field>
-      <v-text-field filled label="Password"></v-text-field>
-      <v-text-field filled label="Birthday"></v-text-field>
-      <v-text-field filled label="Full Name"></v-text-field>
-      <v-textarea
-       filled
-       label="About me"></v-textarea>
+
+      <v-row no-gutters>
+        <v-col
+        cols="12"
+        lg="6"
+        class="pa-8" >
+          <v-text-field filled label="Username"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row
+        :align="align"
+        no-gutters >
+        <v-col
+          cols="12"
+          lg="6"
+          class="pa-8" >
+          <v-text-field filled label="Password"></v-text-field>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col
+          cols="12"
+          lg="6"
+          class="pa-8" >
+          <v-text-field filled label="Birthday"></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row
+        :align="align"
+        no-gutters >
+        <v-col
+          class="pa-8" >
+          <v-text-field filled label="Full Name"></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row
+        :align="align"
+        no-gutters >
+        <v-col
+          class="pa-8" >
+          <v-textarea
+           filled
+           label="About me"></v-textarea>
+         </v-col>
+       </v-row>
 
       <div class="text-center">
          <v-btn
            height=84
            width=266
-           color="primary"
-           dark
+           color="#0D1A8F"
          >
          Register
          </v-btn>
@@ -37,3 +76,21 @@
     </v-card>
   </v-container>
 </template>
+
+<style>
+.hometext {
+  border-right: 70px solid transparent;
+  border-left: 70px solid transparent;
+  border-top: 15px solid transparent;
+  border-bottom: 15px solid transparent;
+}
+
+.buttonregister {
+  background-color: #0D1A8F;
+}
+
+.bigrect {
+  border-top: 120px solid transparent;
+  border-bottom: 120px solid transparent;
+}
+</style>
